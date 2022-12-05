a5dad35fa1b5bef0375711f3a218a9652b8b6f69
c14 <c14@pop-os.localdomain>
2022-12-05 12:23:33 +0100

    wth

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+mod fs;
+use fuse::mount;
+use std::ffi::OsStr;
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+    let mountpoint = env::args_os().nth(1).unwrap();
+    let options = ["-o", "ro", "-o", "fsname=hello"]
+        .iter()
+        .map(|o| o.as_ref())
+        .collect::<Vec<&OsStr>>();
+    fuse::mount(Q1FS, mountpoint, &options).unwrap();
 }
